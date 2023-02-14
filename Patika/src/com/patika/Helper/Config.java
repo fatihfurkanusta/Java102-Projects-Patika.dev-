3ff36cf40fec854ca8ac7ec5fbd613a8df82554d
Fatih Furkan Usta <fatihfurkanusta@outlook.com>
2023-02-15 01:25:44 +0300

    new commit

--- Patika/src/com/patika/Helper/Config.java
+++
@@ -1,9 +0,0 @@
-package com.patika.Helper;
-
-public class Config {
-    public static final String PROJECT_TITLE = "Patika.Dev";
-    public static final String DATA_URL = "jdbc:postgresql://localhost:5432/patika";
-    public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "postgresql";
-
-}
